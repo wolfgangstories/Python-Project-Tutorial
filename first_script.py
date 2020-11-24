5087bf2278fc7d7861bed16685ca0aa0a0d8c37a
Wolfgang Stories <wolfgangstories@gmail.com>
2020-11-24 22:46:27 +0700

    #1 Python Code with VS Code

---
+++ first_script.py
@@ -0,0 +1,20 @@
+import sys
+import math
+from os import rename
+import requests
+
+name = input("Your name? ")
+print("Hello",name,"!")
+
+#print(sys.version)
+#print(sys.executable)
+
+#def greet(who_to_greet):
+#    greeting = 'Hello, {}'.format(who_to_greet)
+#    return greeting
+
+#r = requests.get('https://www.google.com/')
+#print(r.status_code)
+
+#print(greet('World'))
+#print(greet('Corey'))
